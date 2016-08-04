c9faeb9011c1e4affab5917bde8a6826373e6b03
James Hornung <hornung.james@gmail.com>
2016-08-04 13:54:05 -0500

    multiple updates

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,42 +1,46 @@
-// Inside the burgers_controller.js file, import the following:
-// express
-// method-override
-// body-parser
-// Create the router for the app, and export the router at the end of your file.
-
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var router = express.Router();
-var burger = require('../models/burger.js');
+var burger = require('../models/burgers.js');
+
+console.log(burger);
+
+// burgers.sync({force: true}).then(function () {
+//   // Table created
+//   return burger.create({
+//     burger_name: 'Big Mac',
+//     devoured: false
+//   });
+// });
 
 router.get('/', function (req, res) {
 	res.redirect('/burgers');
 });
 
 router.get('/burgers', function (req, res) {
-	burger.all(function (data) {
+	burger.findAll({}).then(function(data){
 		var hbsObject = { burgers: data };
-		// console.log(hbsObject);
+		console.log(hbsObject);
 		res.render('index', hbsObject);
-	});
+	}); 
 });
 
-router.post('/burgers/create', function(req, res){
-	console.log(req.body.burger_name, " =entered name for burger")
-	burger.create('burger_name', req.body.burger_name, function(){
-		res.redirect('/burgers');
-	});
-});
+// router.post('/burgers/create', function(req, res){
+// 	console.log(req.body.burger_name, " =entered name for burger")
+// 	burger.create({burger_name: req.body.burger_name}, {devoured: req.body.devoured}).then(function(data){
+// 		res.redirect('/burgers');
+// 	})
+// });
 
-router.put('/burgers/update/:id', function (req, res) {
-	var condition = 'id = ' + req.params.id;
+// router.put('/burgers/update/:id', function (req, res) {
+// 	var condition = 'id = ' + req.params.id;
 
-	console.log('condition', condition);
+// 	console.log('condition', condition);
 
-	burger.update({ devoured: req.body.devoured }, condition, function () {
-		res.redirect('/burgers');
-	});
-});
+// 	burger.update({ devoured: req.body.devoured }, condition, function () {
+// 		res.redirect('/burgers');
+// 	});
+// });
 
 module.exports = router;
