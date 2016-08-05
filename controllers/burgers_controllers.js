028a9e136d17f49e43318f123b5e9b7b2d3e5d14
James Hornung <hornung.james@gmail.com>
2016-08-05 10:50:30 -0500

    functional on local host

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -26,21 +26,26 @@ router.get('/burgers', function (req, res) {
 	}); 
 });
 
-// router.post('/burgers/create', function(req, res){
-// 	console.log(req.body.burger_name, " =entered name for burger")
-// 	burger.create({burger_name: req.body.burger_name}, {devoured: req.body.devoured}).then(function(data){
-// 		res.redirect('/burgers');
-// 	})
-// });
+router.post('/burgers/create', function(req, res){
+	console.log(req.body.burger_name, " =entered name for burger")
+	burger.create({burger_name: req.body.burger_name}, {devoured: req.body.devoured}).then(function(data){
+		res.redirect('/burgers');
+	})
+});
 
-// router.put('/burgers/update/:id', function (req, res) {
-// 	var condition = 'id = ' + req.params.id;
+router.put('/burgers/update/:id', function (req, res) {
+	var condition = req.params.id;
+	console.log('condition', condition);
 
-// 	console.log('condition', condition);
-
-// 	burger.update({ devoured: req.body.devoured }, condition, function () {
-// 		res.redirect('/burgers');
-// 	});
-// });
+	burger.update({
+        devoured: req.body.devoured
+    }, {
+        where: {
+            id: condition
+        }
+    }).then(function() {
+        res.redirect('/');
+    });
+});
 
 module.exports = router;
